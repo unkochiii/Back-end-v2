61a2c4fb2d11b9595793b1a8cce8c50fac3d989a
JUNAA MEDIA <junaamedia@MacBook-Pro-de-JUNAA.local>
2025-12-09 14:24:35 +0100

    user update modification, user avatar upload with cloudinary assets

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ const avisRouter = require("./routes/avis");
 
 mongoose.connect(process.env.MONGODB_URI);
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 app.get("/", (req, res) => {
   res.json({ message: "We are in !" });
 });
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const cloudinary = require("cloudinary").v2;
+const fileUpload = require("express-fileupload");
 
 const isAuthenticated = require("../middleware/isAuthenticated");
 const User = require("../models/User");
+const convertToBase64 = require("../utils/convertToBase64");
 
 // READ USER INFO
 router.get("/user/profile", isAuthenticated, (req, res) => {
@@ -32,60 +35,99 @@ router.post("/user/update", isAuthenticated, async (req, res) => {
   const userID = req.user._id;
 
   try {
+    // FIND USER BY USER ID
     const userUpdate = await User.findById(userID);
 
-    if (fullname) {
-      userUpdate.fullname = fullname;
-    }
-    if (email) {
-      userUpdate.email = email;
-    }
-    if (firstBookTitle) {
-      userUpdate.favBooks.firstBook.title = firstBookTitle;
-    }
-    if (firstBookTitle) {
-      userUpdate.favBooks.firstBook.title = firstBookTitle;
-    }
-    if (secondBookTitle) {
-      userUpdate.favBooks.secondBook.title = firstBookTitle;
-    }
-    if (firstBookAuthor) {
+    // UPDATE USER INFO
+    if (fullname) userUpdate.fullname = fullname;
+
+    if (email) userUpdate.email = email;
+
+    if (firstBookTitle) userUpdate.favBooks.firstBook.title = firstBookTitle;
+
+    if (firstBookTitle) userUpdate.favBooks.firstBook.title = firstBookTitle;
+
+    if (secondBookTitle) userUpdate.favBooks.secondBook.title = firstBookTitle;
+
+    if (firstBookAuthor)
       userUpdate.favBooks.firstBook.author_name = firstBookAuthor;
-    }
-    if (secondBookAuthor) {
+
+    if (secondBookAuthor)
       userUpdate.favBooks.secondBook.author_name = secondBookAuthor;
-    }
-    if (firstStyle) {
-      userUpdate.style.firstStyle = firstStyle;
-    }
-    if (secondStyle) {
-      userUpdate.style.secondStyle = secondStyle;
-    }
-    if (thirdStyle) {
-      userUpdate.style.thirdStyle = thirdStyle;
-    }
-    if (birth) {
-      userUpdate.birth = birth;
-    }
-    if (genre) {
-      userUpdate.genre = genre;
-    }
-    if (country) {
-      userUpdate.country = genre;
-    }
-    if (city) {
-      userUpdate.city = city;
-    }
-    if (description) {
-      userUpdate.description = description;
-    }
+
+    if (firstStyle) userUpdate.style.firstStyle = firstStyle;
+
+    if (secondStyle) userUpdate.style.secondStyle = secondStyle;
+
+    if (thirdStyle) userUpdate.style.thirdStyle = thirdStyle;
+
+    if (birth) userUpdate.birth = birth;
+
+    if (genre) userUpdate.genre = genre;
+
+    if (country) userUpdate.country = genre;
+
+    if (city) userUpdate.city = city;
+
+    if (description) userUpdate.description = description;
 
     await userUpdate.save();
 
-    res.status(201).json(userUpdate);
+    res.status(201).json({
+      message: "User successfully updated.",
+      user: { account: { username: userUpdate.account.username } },
+    });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({
+      message: "User hasn't been updated...",
+      errorMessage: error.message,
+    });
   }
 });
+
+// USER AVATAR UPLOAD
+router.post(
+  "/user/avatar/upload",
+  isAuthenticated,
+  fileUpload(),
+  async (req, res) => {
+    const userID = req.user._id;
+    try {
+      // FIND USER BY USER ID
+      const userAvatarUpload = await User.findById(userID);
+
+      // GET AVATAR & UPLOAD ON CLOUDINARY
+      if (req.files) {
+        const fileToString = convertToBase64(req.files.image);
+        const cloudinaryResponse = await cloudinary.uploader.upload(
+          fileToString,
+          { asset_folder: `/${userAvatarUpload.account.username}/avatar` }
+        );
+        // UPDATE AVATAR ON USER ACCOUNT
+        if (cloudinaryResponse)
+          userAvatarUpload.account.avatar = cloudinaryResponse;
+      } else {
+        return res.status(406).json({ message: "Please upload an image !" });
+      }
+
+      await userAvatarUpload.save();
+
+      res.status(201).json({
+        message: "Avatar has been updated !",
+        user: {
+          account: {
+            username: userAvatarUpload.account.username,
+            avatar: userAvatarUpload.account.avatar,
+          },
+        },
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: "Avatar hasn't been updated...",
+        errorMessage: error.message,
+      });
+    }
+  }
+);
 
 module.exports = router;
